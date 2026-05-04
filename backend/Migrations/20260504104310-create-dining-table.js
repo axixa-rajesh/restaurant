3fb9137fb94bd9f1ac8896d6ba266f349bc2046a
khatrikhush <your-github-email>
2026-05-04 17:30:43 +0530

    student 3 day 1 and 2 work complete

---
+++ backend/Migrations/20260504104310-create-dining-table.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('DiningTables', {
+      table_id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      table_number: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING
+      },
+      capacity: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      status: {
+        allowNull: false,
+        defaultValue: 'available',
+        type: Sequelize.STRING
+      },
+      location_note: {
+        allowNull: false, 
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.NOW
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.NOW
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('DiningTables');
+  }
+};
