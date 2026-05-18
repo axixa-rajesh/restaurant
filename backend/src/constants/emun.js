8ae75199742c9d0e3ab6db1223ed111ba374eeb4
varunderasari <varunderasari684@gmail.com>
2026-05-18 23:00:54 +0530

    emun constants created

---
+++ backend/src/constants/emun.js
@@ -0,0 +1,49 @@
+module.export = {
+    Status:['active',"inactive"],
+
+     TABLE_STATUS: [
+    "available",
+    "occupied",
+    "reserved",
+    "cleaning",
+  ],
+
+  ORDER_TYPE: [
+    "dine_in",
+    "takeaway",
+  ],
+
+  ORDER_STATUS: [
+    "pending",
+    "preparing",
+    "ready",
+    "completed",
+    "cancelled",
+  ],
+
+  ITEM_STATUS: [
+    "pending",
+    "preparing",
+    "ready",
+    "served",
+    "packed",
+  ],
+
+  PAYMENT_STATUS: [
+    "unpaid",
+    "partial",
+    "paid",
+  ],
+
+  PAYMENT_METHOD: [
+    "cash",
+    "card",
+    "upi",
+  ],
+
+  PAYMENT_RESULT: [
+    "success",
+    "failed",
+    "refunded",
+  ],
+}
