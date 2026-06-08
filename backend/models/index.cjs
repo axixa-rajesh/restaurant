335dffa8f5510bed08f2de659e79436e4d50f980
khatrikhush <your-github-email>
2026-06-08 14:42:07 +0530

    done

--- backend/models/index.cjs
+++ backend/models/index.cjs
@@ -20,10 +20,10 @@ fs
   .readdirSync(__dirname)
   .filter(file => {
     return (
-      file.indexOf('.') !== 0 &&
+      file.indexOf(".") !== 0 &&
       file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+      file.slice(-4) === ".cjs" &&
+      file.indexOf(".test.js") === -1
     );
   })
   .forEach(file => {
